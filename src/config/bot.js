92324358f9abb65478ed881e133f35610510fe0d
F1reB0Oms <skt.st4r0@gmail.com>
2026-06-28 23:08:33 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Test Community - Helpful bot online 24/7,
+        name: "Test Community - Helpful bot online 24/7",
         // Activity type number (0 = Playing).
         type: 3,
       },
